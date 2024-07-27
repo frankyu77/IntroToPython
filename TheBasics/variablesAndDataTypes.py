32dbe5e45b4255bd1487a5b9488e87bac8b149cb
Frank Yu <kyu16@students.cs.ubc.ca>
2024-07-27 16:58:33 -0700

    first couple lessons

---
+++ TheBasics/variablesAndDataTypes.py
@@ -0,0 +1,14 @@
+
+character_name = "Tom"
+character_age = "50"    # string
+character_age1 = 50     # number
+character_age2 = 50.2342    # floats/long, still numbers
+isMale = False      # boolean
+
+print("There once was a man named " + character_name + ", ")
+print("he was " + character_age + " years old. ")
+
+character_name = "Mike"
+
+print("He really liked the name " + character_name + ", ")
+print("but didn't like being " + character_age + ".")
