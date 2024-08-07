807533cc65226fabce449ed972bc69a23b395a3a
Frank Yu <kyu16@students.cs.ubc.ca>
2024-08-07 16:04:08 -0700

    finished course

---
+++ Object Oriented Programming/Inheritance/app.py
@@ -0,0 +1,10 @@
+from Chef import Chef
+from ChineseChef import ChineseChef
+
+myChef = Chef()
+myChef.make_special_dish()
+
+chineseChef = ChineseChef()
+chineseChef.make_chicken()
+chineseChef.make_fried_rice()
+chineseChef.make_special_dish()
